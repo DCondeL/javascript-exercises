c04db2734e54ecbe02cbfe3f1d40309ea2dbe7ca
dconde <71786473+DCondeL@users.noreply.github.com>
2025-05-23 17:32:24 +0200

    Add solution to remove multiple of the same valueç

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arrToMdf = args[0];
+    let elsToDel = args.slice(1);
+    console.log("Original Array: " + arrToMdf);
+    elsToDel.map((element)=>{
+        let idx = arrToMdf.indexOf(element);
+        console.log("Indice a eliminar: " + idx);
+        arrToMdf.splice(idx, 1);
+        console.log("Array: " + arrToMdf);
+        while( (idx = arrToMdf.indexOf(element, idx) !== -1)){
+            idx = arrToMdf.indexOf(element, idx);
+            console.log("Indice a eliminar: " + idx);
+            arrToMdf.splice(idx, 1);
+            console.log("Array: " + arrToMdf);
+        }
+    });
+    return arrToMdf;
 };
 
 // Do not edit below this line
