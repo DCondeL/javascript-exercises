ec2356f56c7511639c6c7ce1c0f6f21d81e6bed7
dconde <71786473+DCondeL@users.noreply.github.com>
2025-05-23 16:19:19 +0200

    Add feature to reverse strings with multiple words

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
-    let strArray = str.split("");
-    return strArray.reverse().join("");
+    let strsArray = str.split(" ");
+
+    if(strsArray.length < 2){
+        let strArray = str.split("");
+        return strArray.reverse().join("");
+    }
+
+    let reversedStr = strsArray.map((str)=> str.split("").reverse().join("")).reverse().join(" ");
+    return reversedStr;
 };
 
 // Do not edit below this line
