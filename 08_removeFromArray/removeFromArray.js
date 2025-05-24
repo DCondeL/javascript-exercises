08c66e740850ae402b19d0ec2b27fc7b842e3574
dconde <71786473+DCondeL@users.noreply.github.com>
2025-05-24 12:46:46 +0200

    Add solution to removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,20 +1,33 @@
-const removeFromArray = function(...args) {
-    let arrToMdf = args[0];
-    let elsToDel = args.slice(1);
-    console.log("Original Array: " + arrToMdf);
-    elsToDel.map((element)=>{
-        let idx = arrToMdf.indexOf(element);
-        console.log("Indice a eliminar: " + idx);
-        arrToMdf.splice(idx, 1);
-        console.log("Array: " + arrToMdf);
-        while( (idx = arrToMdf.indexOf(element, idx) !== -1)){
-            idx = arrToMdf.indexOf(element, idx);
-            console.log("Indice a eliminar: " + idx);
-            arrToMdf.splice(idx, 1);
-            console.log("Array: " + arrToMdf);
-        }
-    });
-    return arrToMdf;
+const removeFromArray = function (...args) {
+
+	let arrToMdf = args[0];
+	let elsToDel = args.slice(1);
+
+	console.log("Original Array: " + arrToMdf);
+
+	elsToDel.map((element) => {
+
+		let idx = arrToMdf.indexOf(element);
+
+		if(idx === -1){
+			return;
+		}
+
+		console.log("Indice a eliminar: " + idx);
+		arrToMdf.splice(idx, 1);
+		console.log("Array: " + arrToMdf);
+
+		while ((idx = arrToMdf.indexOf(element, idx) !== -1)) {
+
+			idx = arrToMdf.indexOf(element, idx);
+			console.log("Indice a eliminar: " + idx);
+			arrToMdf.splice(idx, 1);
+			console.log("Array: " + arrToMdf);
+
+		}
+
+	});
+	return arrToMdf;
 };
 
 // Do not edit below this line
