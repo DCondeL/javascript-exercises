cdeb2b6def4c04c032f5fb33b97142045ecb8afe
dconde <71786473+DCondeL@users.noreply.github.com>
2025-05-24 13:24:12 +0200

    Add first version of sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...range) {
 
+    range.sort((numA, numB)=> numA-numB);
+    let endOfSum = range[1];
+    let nextValueToSum = range[0]+1;
+
+    while(nextValueToSum < endOfSum){
+
+        range.splice(1, 0, nextValueToSum);
+        nextValueToSum++;
+
+    }
+
+    let sum = range.reduce((acc, value)=> acc+=value);
+    return sum;
 };
 
 // Do not edit below this line
